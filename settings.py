1f82634212ec7c40be4f77557d7b9db43420e580
Chris Latham <cl@bink.com>
2017-01-20 16:59:29 +0000

    unique secret key

--- settings.py
+++ settings.py
@@ -1,10 +1,9 @@
 from environment import env_var, read_env
 
 
-SECRET_KEY = b'\x00\x8d\xab\x02\x88\\\xc2\x96&\x0b<2n0n\xc9\x19\xec8\xab\xc5\x08N['
+SECRET_KEY = b'\xa2\xaeY9>\xda=;\xcc\x7f\x05\xf6\x94.\x93~\xb16\x8e%2\x01\x83\x10'
 
 read_env()
 
 DEV_HOST = env_var("DEV_HOST", "0.0.0.0")
 DEV_PORT = env_var("DEV_PORT", "5050")
-
