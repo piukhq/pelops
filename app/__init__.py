ea037d3b1ac9cace91621cbc080bb6c092f4cde5
Cailet Latham <cailet@bink.com>
2018-11-12 18:21:37 +0000

    Added Datadog APM

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 
+from ddtrace import tracer
+from ddtrace.contrib.flask import TraceMiddleware
+
 
 def create_app(config_name="settings"):
     from app.apis import stub_api
 
     app = Flask('core')
     app.config.from_object(config_name)
+
+    TraceMiddleware(
+        app,
+        tracer,
+        service="pelops",
+        distributed_tracing=True)
 
     stub_api.init_app(app)
 
