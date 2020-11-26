6faa7ef52d435a737a18be0fb786deb4f1aedffe
Chris Pressland <mail@cpressland.io>
2020-11-26 12:10:50 +0000

    Project Cleanup, #DevOpsDelivers

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7-alpine
+FROM binkhq/python:3.8
 
 WORKDIR /app
 ADD . .
@@ -6,4 +6,5 @@ ADD . .
 RUN pip install pipenv gunicorn && \
     pipenv install --system --deploy --ignore-pipfile
 
-CMD ["/usr/local/bin/gunicorn", "-c", "gunicorn.py", "wsgi:app"]
+CMD [ "gunicorn", "--workers=2", "--threads=2", "--error-logfile=-", \
+                  "--access-logfile=-", "--bind=0.0.0.0:9000", "wsgi:app" ]
--- app/apis/spreedly_stubs.py
+++ app/apis/spreedly_stubs.py
@@ -1,19 +1,18 @@
 import json
 from uuid import uuid4
 
-from flask import request, jsonify, Response
+from flask import Response, jsonify, request
 from flask_restplus import Namespace, Resource
 
 from app.apis.storage import Redis
 from app.fixtures.spreedly import deliver_data, export_data
-from settings import REDIS_URL
-from settings import logger
+from settings import REDIS_URL, logger
 
-spreedly_api = Namespace('spreedly', description='Spreedly related operations')
+spreedly_api = Namespace("spreedly", description="Spreedly related operations")
 storage = Redis(url=REDIS_URL)
 
 DEFAULT_FILE_DATA = {"payment_tokens": [], "transaction_tokens": [], "void_fail_tokens": []}
-PAYMENT_TOKEN_FILEPATH = 'app/fixtures/payment.json'
+PAYMENT_TOKEN_FILEPATH = "app/fixtures/payment.json"
 VOID_FAILURE_FLAG = "voidfail"
 
 
@@ -22,54 +21,52 @@ def spreedly_token_response(transaction_token, has_succeeded):
         "transaction": {
             "token": transaction_token,
             "succeeded": has_succeeded,
-            "response": {
-                "message": "",
-                "error_code": 0
-            }
+            "response": {"message": "", "error_code": 0},
         }
     }
 
 
-@spreedly_api.route('/receivers/<token>/deliver.xml')
+@spreedly_api.route("/receivers/<token>/deliver.xml")
 class Deliver(Resource):
     def post(self, token):
         data = request.get_json()
         logger.info(f"request  /receivers/{token}/deliver.xml body:{data}")
         if token in deliver_data:
-            return Response(deliver_data[token], mimetype='text/xml')
+            return Response(deliver_data[token], mimetype="text/xml")
         else:
-            spreedly_api.abort(404, 'No deliver data for token {}'.format(token))
+            spreedly_api.abort(404, "No deliver data for token {}".format(token))
 
 
-@spreedly_api.route('/receivers/<token>/deliver.json')
+@spreedly_api.route("/receivers/<token>/deliver.json")
 class DeliverJson(Resource):
     def post(self, token):
         data = request.get_json()
         logger.info(f"request /receivers/{token}/deliver.json  body: {data}")
-        delivery = data.get('delivery', {})
-        pay_token = delivery.get('payment_method_token', "")
-        if token == 'visa' and len(pay_token) > 7 and pay_token[0:7] == 'ERRADD_':
+        delivery = data.get("delivery", {})
+        pay_token = delivery.get("payment_method_token", "")
+        if token == "visa" and len(pay_token) > 7 and pay_token[0:7] == "ERRADD_":
             error_return = pay_token[7:]
-            resp_data = deliver_data['visa_error']
-            resp_data["transaction"]["response"]["body"] = resp_data["transaction"]["response"]["body"]\
-                .replace("<<error>>", error_return)
-            return Response(json.dumps(resp_data), mimetype='application/json')
+            resp_data = deliver_data["visa_error"]
+            resp_data["transaction"]["response"]["body"] = resp_data["transaction"]["response"]["body"].replace(
+                "<<error>>", error_return
+            )
+            return Response(json.dumps(resp_data), mimetype="application/json")
         if token in deliver_data:
-            return Response(json.dumps(deliver_data[token]), mimetype='application/json')
+            return Response(json.dumps(deliver_data[token]), mimetype="application/json")
         else:
-            spreedly_api.abort(404, 'No deliver data for token {}'.format(token))
+            spreedly_api.abort(404, "No deliver data for token {}".format(token))
 
 
-@spreedly_api.route('/receivers/<token>/export.xml')
+@spreedly_api.route("/receivers/<token>/export.xml")
 class Export(Resource):
     def post(self, token):
         if token in export_data:
-            return Response(export_data[token], mimetype='text/xml')
+            return Response(export_data[token], mimetype="text/xml")
         else:
-            spreedly_api.abort(404, 'No export data for token {}'.format(token))
+            spreedly_api.abort(404, "No export data for token {}".format(token))
 
 
-@spreedly_api.route('/payment_methods/<token>/retain.json')
+@spreedly_api.route("/payment_methods/<token>/retain.json")
 class Retain(Resource):
     def put(self, token):
         #  data = request.get_json()
@@ -77,50 +74,43 @@ class Retain(Resource):
         if token:
             return True
         else:
-            spreedly_api.abort(404, 'Not retained token {}'.format(token))
-            logger.info(f"Spreedly api Abort 404 - no retained token")
+            spreedly_api.abort(404, "Not retained token {}".format(token))
+            logger.info("Spreedly api Abort 404 - no retained token")
 
 
-@spreedly_api.route('/v1/gateways/<gateway_token>/purchase.json')
+@spreedly_api.route("/v1/gateways/<gateway_token>/purchase.json")
 class PaymentPurchase(Resource):
     def post(self, gateway_token):
         request_json = request.get_json()
 
         try:
-            input_payment_token = request_json['transaction']['payment_method_token']
+            input_payment_token = request_json["transaction"]["payment_method_token"]
         except KeyError:
             return jsonify({"message": "incorrect input format"})
 
-        token_storage_key = 'tokens'
+        token_storage_key = "tokens"
         try:
             file_data = json.loads(storage.get(token_storage_key))
         except (json.JSONDecodeError, storage.NotFound):
             file_data = DEFAULT_FILE_DATA
 
-        if input_payment_token in file_data['payment_tokens']:
+        if input_payment_token in file_data["payment_tokens"]:
             transaction_token = str(uuid4())
             resp = spreedly_token_response(transaction_token, has_succeeded=True)
-            file_data = {
-                "payment_tokens": [],
-                "transaction_tokens": [transaction_token],
-                "void_fail_tokens": []
-            }
+            file_data = {"payment_tokens": [], "transaction_tokens": [transaction_token], "void_fail_tokens": []}
             if input_payment_token == VOID_FAILURE_FLAG:
-                file_data['void_fail_tokens'] = [transaction_token]
-            storage.set(
-                token_storage_key,
-                json.dumps(file_data)
-            )
+                file_data["void_fail_tokens"] = [transaction_token]
+            storage.set(token_storage_key, json.dumps(file_data))
         else:
             resp = spreedly_token_response(str(uuid4()), has_succeeded=False)
 
         return jsonify(resp)
 
 
-@spreedly_api.route('/v1/transactions/<transaction_token>/void.json')
+@spreedly_api.route("/v1/transactions/<transaction_token>/void.json")
 class PaymentVoid(Resource):
     def post(self, transaction_token):
-        token_storage_key = 'tokens'
+        token_storage_key = "tokens"
         try:
             file_data = json.loads(storage.get(token_storage_key))
         except (json.JSONDecodeError, storage.NotFound):
@@ -128,37 +118,21 @@ class PaymentVoid(Resource):
 
         void_fail = transaction_token in file_data["void_fail_tokens"]
         if transaction_token in file_data["transaction_tokens"] and not void_fail:
-            resp = {
-                "transaction": {
-                    "succeeded": True,
-                    "response": {
-                        "message": "",
-                        "error_code": 0
-                    }
-                }
-            }
+            resp = {"transaction": {"succeeded": True, "response": {"message": "", "error_code": 0}}}
         else:
-            resp = {
-                "transaction": {
-                    "succeeded": False,
-                    "response": {
-                        "message": "",
-                        "error_code": 0
-                    }
-                }
-            }
+            resp = {"transaction": {"succeeded": False, "response": {"message": "", "error_code": 0}}}
 
         return jsonify(resp)
 
 
-@spreedly_api.route('/add_payment_token')
+@spreedly_api.route("/add_payment_token")
 class AddPaymentToken(Resource):
     def post(self):
         try:
-            tokens = request.get_json()['payment_tokens']
+            tokens = request.get_json()["payment_tokens"]
         except KeyError:
             return jsonify({"message": "incorrect input format"})
-        token_storage_key = 'tokens'
+        token_storage_key = "tokens"
         tokens_added = []
         errors = []
 
@@ -171,16 +145,17 @@ class AddPaymentToken(Resource):
             file_data = {"payment_tokens": [], "transaction_tokens": []}
 
         for token in tokens:
-            if token not in file_data['payment_tokens']:
-                file_data['payment_tokens'].append(token)
+            if token not in file_data["payment_tokens"]:
+                file_data["payment_tokens"].append(token)
                 tokens_added.append(token)
             else:
                 errors.append("payment token '{}' already in valid tokens list".format(token))
 
         storage.set(
             token_storage_key,
-            json.dumps({"payment_tokens": file_data['payment_tokens'],
-                       "transaction_tokens": file_data['transaction_tokens']})
+            json.dumps(
+                {"payment_tokens": file_data["payment_tokens"], "transaction_tokens": file_data["transaction_tokens"]}
+            ),
         )
 
         return jsonify({"tokens added": tokens_added, "errors": errors})
